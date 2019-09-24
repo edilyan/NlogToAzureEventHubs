8dbfb73a7cf2fe223c4debac30caf3fc010d210d
Suren Edilyan <surene@dom-daniel.com>
2019-09-24 11:51:58 +0400

    New Solution: WebAppDataTravel

---
+++ WebAppDataTravel/WebAppDataTravel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppDataTravel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
